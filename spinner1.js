780f6a82b742c66e35389d1598285949eb9a16b3
Umang Patel <umangpatel1991.cmail.com>
2021-09-21 14:48:28 -0400

    Instead of looping through each key, used undividual setTimeout to print keys

--- spinner1.js
+++ spinner1.js
@@ -1,8 +1,36 @@
-const spinnerKeys = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|',];
-let counter = 100;
-for (let key of spinnerKeys) {
-  setTimeout(() => {
-    process.stdout.write(`\r${key}   `);
-  }, counter);
-  counter += 200;
-}
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   ');
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/  ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-  ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1700);
